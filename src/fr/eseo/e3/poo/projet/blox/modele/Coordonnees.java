1a1bf7909addd777e95ba0de6618c4dce7879104
ChocoScaff <anjul2@hotmail.fr>
2024-04-22 10:30:46 +0200

    create class Coordonnees

---
+++ src/fr/eseo/e3/poo/projet/blox/modele/Coordonnees.java
@@ -0,0 +1,52 @@
+package fr.eseo.e3.poo.projet.blox.modele;
+
+/**
+ * @class Coordonnees
+ */
+public class Coordonnees {
+	private int abscisse;
+	private int ordonnee;
+	
+	/**
+	 * 
+	 * @param abscisse
+	 * @param ordonnee
+	 */
+	public Coordonnees(int abscisse, int ordonnee) {
+		this.abscisse = abscisse;
+		this.ordonnee = ordonnee;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getAbscisse() {
+		return this.abscisse;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getOrdonnee() {
+		return this.ordonnee;
+	}
+	
+	/**
+	 * 
+	 * @param abscisse
+	 */
+	public void setAbscisse(int abscisse) {
+		this.abscisse = abscisse;
+	}
+	
+	/**
+	 * 
+	 * @param ordonnee
+	 */
+	public void setOrdonnee(int ordonnee) {
+		this.ordonnee = ordonnee;
+	}
+	
+}
