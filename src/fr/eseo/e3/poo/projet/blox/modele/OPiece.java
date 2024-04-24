22e5bf5b72fef551432ea8fbe5af41cb03ba15f5
ChocoScaff <anjul2@hotmail.fr>
2024-04-24 14:47:10 +0200

    Add package pieces

--- src/fr/eseo/e3/poo/projet/blox/modele/OPiece.java
+++
@@ -1,8 +0,0 @@
-package fr.eseo.e3.poo.projet.blox.modele;
-
-public class OPiece {
-	public OPiece(Coordonnees coordonnees, Couleur couleur) {
-			
-		
-	}
-}
---
+++ src/fr/eseo/e3/poo/projet/blox/modele/pieces/OPiece.java
@@ -0,0 +1,10 @@
+package fr.eseo.e3.poo.projet.blox.modele.pieces;
+
+import fr.eseo.e3.poo.projet.blox.modele.*;
+
+public class OPiece {
+	public OPiece(Coordonnees coordonnees, Couleur couleur) {
+			
+		
+	}
+}
