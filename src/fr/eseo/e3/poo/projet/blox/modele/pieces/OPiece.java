3a7bf05294277164256a2c89b00f649b48e9870b
ChocoScaff <anjul2@hotmail.fr>
2024-04-24 14:30:43 +0200

    Change

---
+++ src/fr/eseo/e3/poo/projet/blox/modele/pieces/OPiece.java
@@ -0,0 +1,12 @@
+package fr.eseo.e3.poo.projet.blox.modele.pieces;
+
+import fr.eseo.e3.poo.projet.blox.modele.Coordonnees;
+import fr.eseo.e3.poo.projet.blox.modele.Couleur;
+
+public class OPiece {
+	public OPiece(Coordonnees coordonnees, Couleur couleur) {
+		
+		
+	}
+}
+
