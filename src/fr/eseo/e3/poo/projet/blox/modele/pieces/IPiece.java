ad921745544593e88a15bf61f6c6be15ef64b887
ChocoScaff <anjul2@hotmail.fr>
2024-04-24 17:12:23 +0200

    add IPiece

---
+++ src/fr/eseo/e3/poo/projet/blox/modele/pieces/IPiece.java
@@ -0,0 +1,16 @@
+package fr.eseo.e3.poo.projet.blox.modele.pieces;
+
+import fr.eseo.e3.poo.projet.blox.modele.*;
+
+public class IPiece extends Piece {
+		
+		/**
+		 * 
+		 * @param coordonnees
+		 * @param couleur
+		 */
+		public IPiece(Coordonnees coordonnees, Couleur couleur) {
+			super(coordonnees, couleur);
+			// TODO Auto-generated constructor stub
+		}
+}
