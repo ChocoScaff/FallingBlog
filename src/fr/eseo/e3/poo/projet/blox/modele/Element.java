b91c92b2e7cd3fd9b5735d94f234e94ffb79e57f
ChocoScaff <anjul2@hotmail.fr>
2024-04-25 09:12:09 +0200

    add equals

--- src/fr/eseo/e3/poo/projet/blox/modele/Element.java
+++ src/fr/eseo/e3/poo/projet/blox/modele/Element.java
@@ -88,7 +88,18 @@ public class Element {
     public String toString() {
         return "(" + this.coordonnees + ", " + this.couleur + ")";
     }
-
+    
+    /**
+     * Override equals() method
+     */
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Element element = (Element) obj;
+        return Objects.equals(coordonnees, element.coordonnees) &&
+               Objects.equals(couleur, element.couleur);
+    }
+    
     /**
      * Override hashCode() method
      * 
