30824f6d6bc749c293727f808da25b2a4877d1ab
ChocoScaff <anjul2@hotmail.fr>
2024-04-25 09:41:40 +0200

    add func in Piece

--- src/fr/eseo/e3/poo/projet/blox/modele/Piece.java
+++ src/fr/eseo/e3/poo/projet/blox/modele/Piece.java
@@ -1,7 +1,13 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
+import java.util.List;
+
 abstract public class Piece {
 
+	private Couleur couleur;
+    private Puits puits;
+    private Element element;
+	
 	/**
 	 * 
 	 * @param coordonnees
@@ -9,5 +15,63 @@ abstract public class Piece {
 	 */
 	public Piece(Coordonnees coordonnees, Couleur couleur) {
 		
+    	this.element.setCoordonnes(coordonnees);
+		this.couleur = couleur;
 	}
+	
+	/**
+	 * 
+	 * @param coordonnees
+	 * @param couleur
+	 */
+	protected void setElements(Coordonnees coordonnees, Couleur couleur) {
+		
+    	Element element = new Element(coordonnees, couleur);
+	    this.element = element;
+	}
+
+	/**
+	 *  Get elements
+	 * @return
+	 */
+	public List<Element> getElements() {
+		return (List<Element>) element;
+	}
+	
+	/**
+	 * Set position
+	 * @param abscisse
+	 * @param ordonnee
+	 */
+    public void setPosition(int abscisse, int ordonnee) {
+    	
+    	Coordonnees coordonnees = new Coordonnees(abscisse, ordonnee);
+    	
+    	this.element.setCoordonnes(coordonnees);
+    }
+
+    /**
+     * Override toString() method
+     */
+    public String toString() {
+        
+        return "(" + this.element + ", " + this.couleur +  ", " + this.puits + ")";
+    }
+
+    /**
+     * Get puits
+     * @return
+     */
+    public Puits getPuits() {
+        return puits;
+    }
+
+    /**
+     * Set puits
+     * @param puits
+     */
+    public void setPuits(Puits puits) {
+        this.puits = puits;
+    }
+	
 }
