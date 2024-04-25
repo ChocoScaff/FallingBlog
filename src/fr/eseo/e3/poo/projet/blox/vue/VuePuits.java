1360a5f363504d01d5178a4b0228e31fc3cfc233
fortune <fortune@fortune.place>
2024-04-25 09:24:32 +0200

    taille et main

--- src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
+++ src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
@@ -1,7 +1,18 @@
 package fr.eseo.e3.poo.projet.blox.vue;
 
+import java.awt.*;
 import javax.swing.JPanel;
 
 public class VuePuits extends JPanel {
-	
+    public VuePuits()
+    {
+        this.setBounds(0, 0, 100, 100);
+        this.setBackground(Color.BLUE);
+    }
+
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.fillRect(0, 0, 50, 50);
+    }
 }
