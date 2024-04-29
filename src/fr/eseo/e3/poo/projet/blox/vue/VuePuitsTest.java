f7646d25b708a714004a6db7369406b3844ceff0
ChocoScaff <anjul2@hotmail.fr>
2024-04-29 17:18:33 +0200

    add piece in puit

--- src/fr/eseo/e3/poo/projet/blox/vue/VuePuitsTest.java
+++ src/fr/eseo/e3/poo/projet/blox/vue/VuePuitsTest.java
@@ -1,5 +1,8 @@
 package fr.eseo.e3.poo.projet.blox.vue;
+import fr.eseo.e3.poo.projet.blox.modele.Coordonnees;
+import fr.eseo.e3.poo.projet.blox.modele.Couleur;
 import fr.eseo.e3.poo.projet.blox.modele.Puits;
+import fr.eseo.e3.poo.projet.blox.modele.pieces.OPiece;
 
 public class VuePuitsTest {
     public static void main(String[] args) {
@@ -7,6 +10,9 @@ public class VuePuitsTest {
         VuePuits vp = new VuePuits(puits);
         vp.DisplayPuits();
 
+        Coordonnees coordonnees = new Coordonnees(2,2);
+        OPiece oPiece = new OPiece(coordonnees, Couleur.ROUGE);
+        puits.setPieceSuivante(oPiece);
     }
 }
 
