badfa9f5bdd45424a3b390cb825ddca56a3253ad
fortune <fortune@fortune.place>
2024-04-29 11:06:41 +0200

    Grid

--- src/fr/eseo/e3/poo/projet/blox/modele/Puits.java
+++ src/fr/eseo/e3/poo/projet/blox/modele/Puits.java
@@ -1,8 +1,8 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
 public class Puits {
-	private static final int LARGEUR_PAR_DEFAULT = 360;
-	private static final int PROFONDEUR_PAR_DEFAULT = 600;
+	private static final int LARGEUR_PAR_DEFAULT = 36;
+	private static final int PROFONDEUR_PAR_DEFAULT = 60;
 
 	private int largeur;
 	private int profondeur;
--- src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
+++ src/fr/eseo/e3/poo/projet/blox/vue/VuePuits.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class VuePuits extends JPanel {
     private Puits puits;
 
-    public static final int TAILLE_PAR_DEFAUT = 700; // assuming a default size
+    public static final int TAILLE_PAR_DEFAUT = 400; // assuming a default size
 
     public VuePuits(Puits puits) {
         this(puits, TAILLE_PAR_DEFAUT);
@@ -15,14 +15,27 @@ public class VuePuits extends JPanel {
 
     public VuePuits(Puits puits, int taille) {
         this.puits = puits;
-        this.setPreferredSize(new Dimension(taille, puits.getProfondeur()));
-        this.setBackground(Color.BLUE);
+        this.setPreferredSize(new Dimension(taille, taille));
+        this.setBackground(Color.WHITE);
     }
 
+    @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        // Using Puits properties for size
-        g.fillRect(0, 0, puits.getLargeur(), puits.getProfondeur());
+        super.paintComponent(g); // This will fill the background with the set color (white)
+
+        // Cast the Graphics object to Graphics2D for better control over the graphics
+        Graphics2D g2D = (Graphics2D) g.create();
+
+        // Drawing a light gray grid
+        g2D.setColor(Color.GRAY);
+        int tileSize = this.getTaille() / puits.getProfondeur();
+        for (int x = 0; x < puits.getLargeur()*tileSize; x += tileSize) {
+            for (int y = 0; y < puits.getProfondeur()*tileSize; y += tileSize) {
+                g2D.drawRect(x, y, tileSize, tileSize);
+            }
+        }
+
+        g2D.dispose(); // Free up the graphics object resources
     }
 
     public Puits getPuits() {
