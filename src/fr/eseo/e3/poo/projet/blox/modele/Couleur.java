a22556b0f98e872d731f8f96f70d986313875bae
fortune <fortune@fortune.place>
2024-04-30 15:37:05 +0200

    New Color

--- src/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
+++ src/fr/eseo/e3/poo/projet/blox/modele/Couleur.java
@@ -1,14 +1,37 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
+import java.awt.Color;
+
 /**
- * @enum couleur
+ * This enumeration defines colors with corresponding java.awt.Color instances for display purposes.
  */
 public enum Couleur {
-	ROUGE,
-	ORANGE,
-	BLEU,
-	VERT,
-	JAUNE,
-	CYAN,
-	VIOLET
+	ROUGE(Color.RED),
+	ORANGE(Color.ORANGE),
+	BLEU(Color.BLUE),
+	VERT(Color.GREEN),
+	JAUNE(Color.YELLOW),
+	CYAN(Color.CYAN),
+	VIOLET(Color.MAGENTA);
+
+	// Instance variable to store the associated java.awt.Color
+	private final Color couleurPourAffichage;
+
+	/**
+	 * Private constructor to initialize the color for each enumeration constant.
+	 *
+	 * @param couleurPourAffichage the java.awt.Color instance associated with the constant
+	 */
+	Couleur(Color couleurPourAffichage) {
+		this.couleurPourAffichage = couleurPourAffichage;
+	}
+
+	/**
+	 * Returns the java.awt.Color object associated with the enum constant.
+	 *
+	 * @return java.awt.Color instance linked with the enum constant.
+	 */
+	public Color getCouleurPourAffichage() {
+		return couleurPourAffichage;
+	}
 }
