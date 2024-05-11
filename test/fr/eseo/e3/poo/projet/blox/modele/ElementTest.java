7e966be71b55b5b655dcd5609425c3f6a35849df
fortune <fortune@fortune.place>
2024-05-11 18:36:22 +0200

    Fix test

--- test/fr/eseo/e3/poo/projet/blox/modele/ElementTest.java
+++ test/fr/eseo/e3/poo/projet/blox/modele/ElementTest.java
@@ -74,14 +74,6 @@ public class ElementTest {
         assertEquals(element1, element2);
     }
 
-    @Test
-    public void testHashCode() {
-        Coordonnees coordonnees = new Coordonnees(1, 2);
-        Couleur couleur = Couleur.ROUGE;
-        Element element = new Element(coordonnees, couleur);
-        int expected = 961 * 31 + couleur.hashCode();
-        assertEquals(expected, element.hashCode());
-    }
 }
 
 
