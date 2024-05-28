4145b166a80e176259f87a7862f89d18d1d3e7d8
KCDeady <arnaud.jacques60@gmail.com>
2024-05-28 09:51:10 +0200

    PieceTest.java terminé, la colision avec le mur avec/sans TAS fonctionnent

---
+++ test/fr/eseo/e3/poo/projet/blox/modele/TasTest.java
@@ -0,0 +1,12 @@
+package fr.eseo.e3.poo.projet.blox.modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TasTest {
+
+    @Test
+    void ajouterElements() {
+    }
+}
